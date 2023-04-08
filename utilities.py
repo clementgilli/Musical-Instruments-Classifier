fff25282686a6c1b752abde7824556d1085f6973
BoubouSW <gilliclement@gmail.com>
2023-04-08 15:18:23 +0200

    add classifier

--- utilities.py
+++ utilities.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from PIL import Image, ImageTk
 import librosa
+import sklearn
+from tqdm.notebook import tqdm, trange
+import time 
+import ipywidgets as widgets
 
 
 def calc_fft(y,sr, maxFreq=-1):
@@ -47,7 +51,7 @@ def load_train(nb_per_class,begin=0,duration=2,maxfreq=5000):
     dic = {}
     l = 0
     flag = True
-    for file_name in df_exp["FileName"]:
+    for file_name in tqdm(df_exp["FileName"]):
         signal, rate = librosa.load("./dataset/Train_submission/Train_submission/"+file_name,offset=begin,duration=duration)
         #newS, newR = extract2s(signal, rate)
         fft1,fft2 = calc_fft(signal,rate, maxFreq=maxfreq)
@@ -63,7 +67,7 @@ def load_train(nb_per_class,begin=0,duration=2,maxfreq=5000):
     df_exp = pd.concat((df_exp, df_prim), axis=1)
     return df_exp
 
-def load_subsets(df,coef_train=0.6, coef_valid=0.2):
+def load_subsets(df,coef_train=0.6, coef_valid=0.4):
     guitar = df[df['Class'] == "Sound_Guitar"]
     drum = df[df['Class'] == "Sound_Drum"]
     violin = df[df['Class'] == "Sound_Violin"]
@@ -71,44 +75,81 @@ def load_subsets(df,coef_train=0.6, coef_valid=0.2):
 
     column_headers = df.columns.values[2:]
     coef_test = 1 - coef_train - coef_valid
-    assert(coef_test>0) ## on vérifie qu'il reste des exemples pour le test set
+    #assert(coef_test>0) ## on vérifie qu'il reste des exemples pour le test set
     Ntot   = len(guitar)
     Ntrain = int(coef_train*Ntot)
     Nvalid = int(coef_valid*Ntot)
     Ntest  = Ntot - Ntrain - Nvalid
     data_train = pd.DataFrame()
     data_valid = pd.DataFrame()
-    data_test = pd.DataFrame()
-    for i in range(Ntrain):
+    #data_test = pd.DataFrame()
+    for i in tqdm(range(Ntrain)):
         # je n ai pas trouve de moyen moins CHIANT de faire ca
         data_train = pd.concat([data_train, pd.DataFrame.transpose(pd.DataFrame(drum.iloc[i]))])
         data_train = pd.concat([data_train, pd.DataFrame.transpose(pd.DataFrame(guitar.iloc[i]))])
         data_train = pd.concat([data_train, pd.DataFrame.transpose(pd.DataFrame(piano.iloc[i]))])
         data_train = pd.concat([data_train, pd.DataFrame.transpose(pd.DataFrame(violin.iloc[i]))])
 
-    for i in range(Ntrain, Ntrain + Nvalid):
+    for i in tqdm(range(Ntrain, Ntrain + Nvalid)):
         data_valid = pd.concat([data_valid, pd.DataFrame.transpose(pd.DataFrame(drum.iloc[i]))])
         data_valid = pd.concat([data_valid, pd.DataFrame.transpose(pd.DataFrame(guitar.iloc[i]))])
         data_valid = pd.concat([data_valid, pd.DataFrame.transpose(pd.DataFrame(piano.iloc[i]))])
         data_valid = pd.concat([data_valid, pd.DataFrame.transpose(pd.DataFrame(violin.iloc[i]))])
 
-    for i in range(Ntrain + Nvalid, Ntot):
-        data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(drum.iloc[i]))])
-        data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(guitar.iloc[i]))])
-        data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(piano.iloc[i]))])
-        data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(violin.iloc[i]))])
+    #for i in range(Ntrain + Nvalid, Ntot):
+    #    data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(drum.iloc[i]))])
+    #    data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(guitar.iloc[i]))])
+    #    data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(piano.iloc[i]))])
+    #    data_test = pd.concat([data_test, pd.DataFrame.transpose(pd.DataFrame(violin.iloc[i]))])
 
     column_headers = df.columns.values[2:]
     X_train = data_train[column_headers]
     y_train = data_train["Class"]
-    X_test = data_test[column_headers]
-    y_test = data_test["Class"]
+    #X_test = data_test[column_headers]
+    #y_test = data_test["Class"]
     X_valid = data_valid[column_headers]
     y_valid = data_valid["Class"]
-    return X_train, y_train, X_valid, y_valid, X_test, y_test
+    return X_train, y_train, X_valid, y_valid #, X_test, y_test
 
-
+def plot_cumexpvar(X_train,varianceExplained=0.95):
+    preProc = sklearn.decomposition.PCA(varianceExplained)
+    preProc.fit(X_train)
+    CumulativeExplainedVariance = np.cumsum(preProc.explained_variance_ratio_)
+    plt.plot(CumulativeExplainedVariance, marker='+')
+    plt.xlabel("Variance")
+    plt.ylabel("n_component")
         
+def plot_score_components(X_train,y_train,X_valid,y_valid,begin,end,step):
 
+    linear_training_score = []
+    linear_valid_score = []
+
+    clf = sklearn.svm.SVC(C=2,kernel="poly",degree=5,coef0=1)
+    nComp_range = np.arange(begin,end,step) # [800,1200] #np.arange(130,175,1) #130 170
+
+    for nC in tqdm(nComp_range):
+        
+        preProc = sklearn.decomposition.PCA(nC)
+        preProc.fit(X_train)
+
+        X_train_Transformed = preProc.transform(X_train)
+        X_valid_Transformed = preProc.transform(X_valid)
+
+        clf.fit(X_train_Transformed, y_train)
+        trainscore = clf.score(X_train_Transformed,y_train)
+        validscore = clf.score(X_valid_Transformed,y_valid)
+        linear_training_score.append(trainscore)
+        linear_valid_score.append(validscore)
+    
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(nComp_range, linear_training_score, label= "train score")
+    ax.plot(nComp_range, linear_valid_score, label= "valid score")
+    ax.set_xlabel("nombre comp")
+    ax.set_ylabel("scores")
+    ax.legend()
+    ax.set_ylim([0.5,1])
+    bestIndex = np.argmax(linear_valid_score) 
+    print(f"best n_comp : {nComp_range[bestIndex]}")
 
 
